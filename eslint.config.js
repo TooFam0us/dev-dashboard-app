b4166289aaa8030bfb2b7458aaf982ddb5c1424e
TooFam0us <jere.syrjanen@gmail.com>
2025-08-13 14:30:44 +0300

    Fixes to jest CI

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,7 @@ import reactPlugin from 'eslint-plugin-react';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import parser from '@typescript-eslint/parser';
 import pluginTs from '@typescript-eslint/eslint-plugin';
+import jestPlugin from 'eslint-plugin-jest';
 import { globalIgnores } from 'eslint/config';
 
 export default [
@@ -41,6 +42,21 @@ export default [
       ...reactHooks.configs['recommended'].rules,
       ...reactRefresh.configs.vite.rules,
       'react/react-in-jsx-scope': 'off', // Not needed with React 17+
+    },
+  },
+  {
+    files: ['**/*.test.ts', '**/*.test.tsx'],
+    plugins: {
+      jest: jestPlugin,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...jestPlugin.environments.globals.globals,
+      },
+    },
+    rules: {
+      ...jestPlugin.configs.recommended.rules,
     },
   },
 ];
